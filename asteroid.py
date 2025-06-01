65e84945e70909535915c743c4312b5c56f58353
addisonelder <addisonelder@tuta.com>
2025-05-31 21:40:07 -0400

    fix: fixed the asteroid update method

--- asteroid.py
+++ asteroid.py
@@ -13,3 +13,6 @@ class Asteroid(CircleShape):
                            radius = self.radius,
                            width = 2,
                            )
+        
+    def update(self, dt):
+        self.position += self.velocity * dt
