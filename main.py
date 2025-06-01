495b1b14fab77dead59b2ec7d219c46e3afd48f6
addisonelder <addisonelder@tuta.com>
2025-05-31 21:03:15 -0400

    refactor: moved player sprite to groups 'drawables' and 'updateables'

--- main.py
+++ main.py
@@ -8,15 +8,24 @@ from player import Player
 def main():
     pygame.init() # initilize the pygame
 
-    # print some stuff for the user
+    # print some info for the user
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # create the background
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # create the clock and init delta time
     clock = pygame.time.Clock()
     dt = 0 # 'delta time': time since last frame has rendered
+
+    # create the pygame groups
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    # set containers for the Player class
+    Player.containers = (updatables, drawables)
 
     # create the ship (player) object
     ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
@@ -30,8 +39,10 @@ def main():
 
         # load the elements that should get rendered
         screen.fill("black")
-        ship.draw(screen)
-        ship.update(dt)
+        for sprite in drawables:
+            sprite.draw(screen)
+        for sprite in updatables:
+            sprite.update(dt)
 
         # render the frame
         pygame.display.flip()
